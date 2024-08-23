a44ca9d8c28e8a973a0e32ec669e2f02fe43682a
Monserrat <rtmonse08@gmail.com>
2024-08-22 20:36:29 -0600

    cambios docker

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from './application/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env. PORT || 3005);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
